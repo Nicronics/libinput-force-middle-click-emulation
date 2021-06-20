98e7959e4293270d9c75f81e86a7c8b1a3fd6f5b
Nicronics <contact@nicronics.com>
2021-06-20 12:43:17 +0200

    Update hook.c

--- hook.c
+++ hook.c
@@ -6,7 +6,7 @@ typedef const char *(*orig_get_name_t)(struct libinput_device *device);
 
 const char *libinput_device_get_name(struct libinput_device *device)
 {
-    libinput_device_config_middle_emulation_set_enabled(device, LIBINPUT_CONFIG_MIDDLE_EMULATION_ENABLED);
+    libinput_device_config_tap_set_enabled(device, LIBINPUT_CONFIG_TAP_ENABLED);
     orig_get_name_t orig_get_name;
     orig_get_name = (orig_get_name_t) dlsym(RTLD_NEXT, "libinput_device_get_name");
     return orig_get_name(device);
